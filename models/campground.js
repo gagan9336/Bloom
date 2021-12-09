bff8566a2727fb99373fb075bfadef124447853e
gagan9336 <deepgagan9336@gmail.com>
2021-12-09 21:27:23 +0530

    first commit

---
+++ models/campground.js
@@ -0,0 +1,28 @@
+var mongoose=require("mongoose");
+//schema Setup
+const campGroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    image_id: String,
+    description: String,
+    categories: String,
+    location: String,
+    lat: Number,
+    lng: Number,
+    author:{
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref:"User"
+        },
+        username:String,
+        createdAt: String
+    },
+    comments:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref :"Comment"
+        }
+    ],
+});
+
+module.exports = mongoose.model("Campground", campGroundSchema);
